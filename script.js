7ed07b3d618f20023494d5126ef3e46f0fb268aa
fire <102771014+Firespread01@users.noreply.github.com>
2024-03-22 10:31:27 -0700

    fix

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function youtube() {
 function tiktok() {
     window.open("https://tiktok.com/@Firespread01")
 }
+function slope() {
+    window.open("firespread01.github.io/slope")
+}
+function cookie() {
+    window.open("firespread01.github.io/cookieclicker")
+}
+
