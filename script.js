306a4873a32d23b9a8e6d6738e37b2a8b32ceae1
fire <102771014+Firespread01@users.noreply.github.com>
2024-09-03 09:54:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function tiktok() {
     window.open("https://tiktok.com/@Firespread01");
 }
 function slope() {
-    window.open("https://firespread01.github.io/slope");
+    window.open('https://firespread01.github.io/slope', '_blank');
 }
 function cookie() {
-    window.open("https://firespread01.github.io/cookieclicker");
+    window.open("https://firespread01.github.io/cookieclicker", '_blank');
 }
 
