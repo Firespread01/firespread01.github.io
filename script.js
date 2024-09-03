879d8030fc8e152d2c499cc68623e7c5efca0565
fire <102771014+Firespread01@users.noreply.github.com>
2024-09-03 09:32:01 -0700

    fix links

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function youtube() {
     window.open("https://youtube.com/@Firespread01");
 }
 function tiktok() {
-    window.open("https://tiktok.com/@Firespread01")
+    window.open("https://tiktok.com/@Firespread01");
 }
 function slope() {
-    window.open("firespread01.github.io/slope")
+    window.open("https://firespread01.github.io/slope");
 }
 function cookie() {
-    window.open("firespread01.github.io/cookieclicker")
+    window.open("https://firespread01.github.io/cookieclicker");
 }
 
